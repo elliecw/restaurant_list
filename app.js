476622f8294610bad1b8b12d7c3795ee71c3c4c5
elliecw <hanchig@gmail.com>
2021-12-08 13:49:41 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -32,10 +32,11 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true })) //改寫成 express
 
 // routes setting
-app.get('/', (req, res) => {
-  Restaurant.find() // 取出 Todo model 裡的所有資料
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id) // 取出 Todo model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-    .then(restaurants => res.render('index', { restaurants })) // 將資料傳給 index 樣板
+    .then(restaurants => res.render('detail', { restaurants })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
 })
 
