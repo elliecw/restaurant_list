94972d995e6f8fadab1c8ff5a5982d4afdeadd04
elliecw <hanchig@gmail.com>
2021-12-11 11:27:38 +0800

    Add CRUD function

--- app.js
+++ app.js
@@ -1,98 +1,107 @@
-// require packages used in the project
 const express = require('express')
-const mongoose = require('mongoose')
 const app = express()
 const port = 3000
-
-// require express-handlebars here
+const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const Restaurant = require('./models/restaurant')
 
-mongoose.connect('mongodb://localhost/restaurant_list') // 設定連線到 mongoDB
-
-// 取得資料庫連線狀態
+mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
-// 連線異常
+
 db.on('error', () => {
   console.log('mongodb error!')
 })
-// 連線成功
+
 db.once('open', () => {
   console.log('mongodb connected!')
 })
 
 // setting template engine
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
-app.set('view engine', 'hbs')
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
 // setting static files
 app.use(express.static('public'))
 
-// setting body-parser
-app.use(express.urlencoded({ extended: true })) //改寫成 express
+app.use(express.urlencoded({ extended: true }))
 
-// routes setting
-app.get('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id) // 取出 Todo model 裡的所有資料
-    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-    .then(restaurants => res.render('detail', { restaurants })) // 將資料傳給 index 樣板
-    .catch(error => console.error(error)) // 錯誤處理
+// get 全部餐廳資料
+app.get('/', (req, res) => {
+  Restaurant.find()
+    .lean()
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.log(error))
 })
 
-app.get('/restaurants/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id) 
-    .lean() 
-    .then(restaurants => res.render('edit', { restaurants })) 
-    .catch(error => console.error(error))
+// Search
+app.get("/search", (req, res) => {
+  if (!req.query.keywords) {
+    res.redirect("/")
+  }
+
+  const keywords = req.query.keywords
+  const keyword = req.query.keywords.trim().toLowerCase()
+
+  Restaurant.find({})
+    .lean()
+    .then(restaurants => {
+      const filterRestaurant = restaurants.filter(
+        data =>
+          data.name.toLowerCase().includes(keyword) ||
+          data.category.includes(keyword)
+      )
+      res.render("index", { restaurants: filterRestaurant, keywords })
+    })
+    .catch(err => console.log(err))
 })
 
-app.get('restaurants/new', (req, res) => {
+// Create
+app.get('/restaurants/new', (req, res) => {
   return res.render('new')
 })
 
 app.post('/restaurants', (req, res) => {
-  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
-  return Restaurant.create({ name })     // 存入資料庫
-    .then(() => res.redirect('/')) // 新增完成後導回首頁
+  const newRestaurant = req.body
+  return Restaurant.create(newRestaurant)
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+// Read detail
+app.get('/restaurants/:id', (req, res) => {
+  const showId = req.params.id
+  return Restaurant.findById(showId)
+    .lean()
+    .then((restaurants) => res.render('show', { restaurants }))
+    .catch(error => console.log(error))
+})
+
+// Edit
+app.get('/restaurants/:id/edit', (req, res) => {
+  const editId = req.params.id
+  return Restaurant.findById(editId)
+    .lean()
+    .then((restaurants) => res.render('edit', { restaurants }))
     .catch(error => console.log(error))
 })
 
 app.post('/restaurants/:id/edit', (req, res) => {
   const id = req.params.id
-  const name = req.body.name
-  return Restaurant.findById(id)
-    .then(restaurant => {
-      restaurant.name = name
-      return restaurant.save()
-    })
-    .then(() => res.redirect(`/restaurants/${id}`))
-    .catch(error => console.log(error))
-})
-
-app.post('/restaurants/:id/delete', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .then(restaurant => restaurant.remove())
+  const editRestaurant = req.body
+  Restaurant.findByIdAndUpdate(id, editRestaurant)
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 
-app.get('/search', (req, res) => {
-  const keyword = req.query.keyword.toLowerCase()
-  const restaurant = restaurants.results.filter(restaurant => {
-    return restaurant.category.toLowerCase().includes(keyword) || restaurant.name.toLowerCase().includes(keyword)
-  })
-  res.render('index', { restaurants: restaurant, keyword: keyword })
+// Delete
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurants => restaurants.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
-app.get('/restaurants/:restaurants_id', (req, res) => {
-  const restaurant = restaurants.results.find(restaurant => restaurant.id.toString() === req.params.restaurants_id)
-  res.render('show', { restaurant: restaurant })
-})
-
-// start and listen on the Express server
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
 })
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,19 +1,17 @@
 const mongoose = require('mongoose')
-const Restaurant = require('../restaurant') // 載入 model
+const Restaurant = require('../restaurant')
+const restaurantList = require('../../restaurant.json').results
 
-mongoose.connect('mongodb://localhost/restaurant_list') // 設定連線到 mongoDB
+mongoose.connect('mongodb://localhost/restaurant-list')
 
-// 取得資料庫連線狀態
 const db = mongoose.connection
-// 連線異常
+
 db.on('error', () => {
   console.log('mongodb error!')
 })
-// 連線成功
+
 db.once('open', () => {
-  console.log('mongodb connected!')
-  for (let i = 0; i < 10; i++) {
-    Restaurant.create({ name: 'name-' + i })
-  }
-  console.log('done')
+  Restaurant.create(restaurantList)
+    .then(() => console.log('restaurantSeeder success'))
+    .catch(error => console.log(error))
 })
