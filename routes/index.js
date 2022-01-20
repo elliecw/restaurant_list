fce77178206bc67a3471f08405088ab549e70d02
elliecw <hanchig@gmail.com>
2022-01-20 15:53:55 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,12 @@ const index = require("./modules/index")
 // 引入模組程式碼
 const Restaurant = require('./modules/restaurant')
 
+const users = require('./modules/users')  // add this
+
 // 將網址結構符合 /restaurants 字串開頭的 request 導向 restaurants 模組 
 router.use("/", index)
 router.use('/restaurants', Restaurant)
+
+router.use('/users', users)  // add this
 
 module.exports = router
