91f4be204584ae7fe19fa1f10ed3afcdf6e206c3
elliecw <hanchig@gmail.com>
2022-01-25 16:59:15 +0800

    feat: add relations between User and Todo models

--- routes/modules/restaurant.js
+++ routes/modules/restaurant.js
@@ -8,16 +8,18 @@ router.get('/new', (req, res) => {
 })
 
 router.post('/', (req, res) => {
+  const userId = req.user._id
   const newRestaurant = req.body
-  return Restaurant.create(newRestaurant)
+  return Restaurant.create(newRestaurant, userId)
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 
 // Read detail
 router.get('/:id', (req, res) => {
-  const showId = req.params.id
-  return Restaurant.findById(showId)
+  const userId = req.user._id
+  const _id = req.params.id
+  return Restaurant.findOne({ _id, userId })
     .lean()
     .then((restaurants) => res.render('show', { restaurants }))
     .catch(error => console.log(error))
@@ -25,15 +27,17 @@ router.get('/:id', (req, res) => {
 
 // Edit
 router.get('/:id/edit', (req, res) => {
-  const editId = req.params.id
-  return Restaurant.findById(editId)
+  const userId = req.user._id
+  const _id = req.params.id
+  return Restaurant.findOne({ _id, userId })
     .lean()
     .then((restaurants) => res.render('edit', { restaurants }))
     .catch(error => console.log(error))
 })
 
 router.put('/:id', (req, res) => {
-  const id = req.params.id
+  const userId = req.user._id
+  const _id = req.params.id
   const editRestaurant = req.body
   Restaurant.findByIdAndUpdate(id, editRestaurant)
     .then(() => res.redirect('/'))
@@ -42,8 +46,9 @@ router.put('/:id', (req, res) => {
 
 // Delete
 router.delete('/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
+  const userId = req.user._id
+  const _id = req.params.id
+  return Restaurant.findOne({ _id, userId })
     .then(restaurants => restaurants.remove())
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
