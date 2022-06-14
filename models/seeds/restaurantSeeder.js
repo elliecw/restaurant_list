2f7972d930a933fdf58198140eaac3fd09347902
elliecw <hanchig@gmail.com>
2022-06-14 23:32:33 +0800

    modify restaurantSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -5,38 +5,44 @@ if (process.env.NODE_ENV !== 'production') {
 const Restaurant = require('../restaurant')
 const User = require('../user')
 const db = require('../../config/mongoose')
+const restaurantList = require('../../restaurant.json').results
+const user = require('../user')
 const SEED_USER = [
   {
     name: 'user1',
     email: 'user1@example.com',
     password: '12345678',
-    restaurantId: [0, 1, 2]
+    restaurantId: [1, 2, 3]
   }, {
     name: 'user2',
     email: 'user2@example.com',
     password: '12345678',
-    restaurantId: [3, 4, 5]
+    restaurantId: [4, 5, 6]
   }]
 
 db.once('open', () => {
-  bcrypt
-    .genSalt(10)
-    .then(salt => bcrypt.hash(SEED_USER.password, salt))
-    .then(hash => User.create({
-      name: SEED_USER.name,
-      email: SEED_USER.email,
-      password: hash
-    }))
-    .then(user => {
-      const seedRestaurant = []
-      SEED_USER.restaurantId.forEach(index => {
-        restaurantList[index].userId = user._id
-        seedRestaurant.push(restaurantList[index])
+  SEED_USER.forEach((user) => {
+    bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(user.password, salt))
+      .then(hash => {
+        User.create({
+          name: user.name,
+          email: user.email,
+          password: hash
+        }).then((userInDB) => {
+          const seedRestaurant = []
+          restaurantList.forEach((restaurant) => {
+            if (user.restaurantId.includes(restaurant.id)) {
+              restaurant["userId"] = userInDB._id
+              seedRestaurant.push(restaurant)
+            }
+          })
+          return Restaurant.create(seedRestaurant)
+        }).then(() => {
+          console.log('done.')
+          process.exit()
+        })
       })
-      return Restaurant.create(seedRestaurant)
-    })
-})
-  .then(() => {
-    console.log('done.')
-    process.exit()
   })
+})
